2d802f398a1e369fe38e64f0db63885ebb6f4e14
Wígny Almeida <wigny.almeida@credisis.com.br>
2021-02-06 16:01:20 -0300

    Configura Tailwind
    https://tailwindcss.com/docs/guides/nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
+
 import { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
