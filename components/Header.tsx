42d9f4c449336f186eb6b7a7fff3f6c524ebb70c
Wígny Almeida <wigny.almeida@credisis.com.br>
2021-02-07 02:38:05 -0300

    Adiciona Cards

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,23 +2,26 @@ import { FC } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 
-export const Header: FC = () => {
-  const menus = [
-    { title: 'Next.js', link: '/nextjs' },
-    { title: 'Tailwind', link: '/tailwind' },
-    { title: 'Framer Motion', link: '/framermotion' },
-  ]
+interface MenuProps {
+  link: string;
+  title: string;
+}
 
+const Menu: FC<MenuProps> = ({ link, title }) => (
+  <Link href={link}>
+    <a className="tracking-wide hover:text-gray-300">{title}</a>
+  </Link>
+)
+
+export const Header: FC = () => {
   return (
     <header className="flex justify-center md:justify-between py-2 md:py-4 items-center">
       <Image src="/img/logo.svg" alt="Logo" width={150} height={33} />
 
       <nav className="hidden md:block space-x-8">
-        {menus.map((m, i) =>
-          <Link href={m.link} key={i}>
-            <a className="tracking-wide hover:text-gray-300">{m.title}</a>
-          </Link>
-        )}
+        <Menu title={'Next.js'} link={'/nextjs'} />
+        <Menu title={'Tailwind'} link={'/tailwind'} />
+        <Menu title={'Framer Motion'} link={'/framermotion'} />
       </nav>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, FC } from 'react';
 import { Layout } from '../components/Layout';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+
+interface CardProps {
+  href: string;
+  imgSrc: string;
+  layoutId: string;
+}
+
+const Card: FC<CardProps> = ({ href, imgSrc, layoutId }) => (
+  <Link href={href}>
+    <div className="relative h-20 md:h-40 bg-gray-200 hover:bg-gray-50 transition rounded-xl cursor-pointer">
+      <motion.img src={imgSrc} layoutId={layoutId} className="w-full h-full" />
+    </div>
+  </Link>
+)
 
 export default class Home extends Component {
   render() {
@@ -8,6 +24,12 @@ export default class Home extends Component {
         <h1 className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">
           Construa sites modernos com agilidade sem precisar sair do HTML.
         </h1>
+
+        <div className="mt-10 md:mt-24 grid w-full grid-cols-1 md:grid-cols-3 grid-rows-3 mdgrid-rows-1 gap-4">
+          <Card href={'/nextjs'} imgSrc={'/img/nextjs.svg'} layoutId={'nextjs-logo'} />
+          <Card href={'/tailwind'} imgSrc={'/img/tailwind.svg'} layoutId={'tailwind-logo'} />
+          <Card href={'/framermotion'} imgSrc={'/img/framermotion.svg'} layoutId={'framermotion-logo'} />
+        </div>
       </Layout>
     );
   }
